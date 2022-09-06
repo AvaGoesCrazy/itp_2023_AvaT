4b707417f32d011567cbb774ab7ab502174c3fb3
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2022-09-06 14:47:02 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -1,7 +1,7 @@
 /*
  * Important facts about the birds of Booleana.
  *
- * Coloration and markings: isFlobbyBird
+ * Coloration and markings: isFlobbyBird && eatsFish
  *
  *   - Flobby Birds and Bloggy Birds are red.
  *   - Flibble Birds and Globby Birds are not red.
