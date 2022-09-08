876341946c5e7a89f0e5c0ae897c99d5272426ab
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2022-09-08 14:57:02 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -39,6 +39,6 @@ const distance = (x, y) => {
   return Math.abs(x - y);
 };
 
-const distance = (x1, y1, x2, y2) => {
+const manhattanDistance = (x1, y1, x2, y2) => {
   return Math.abs(x1 - x2)+ Math.abs(y1 - y2);
 };
