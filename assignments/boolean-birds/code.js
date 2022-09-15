87a69c1b935575f15ce1551e8ef8eeb024557746
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2022-09-15 15:15:20 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -58,3 +58,7 @@ const isSpotted = (birdType) => {
 const isNotRed = (birdType) => {
   return birdType === 'Flibble' || birdType === 'Globby';
 }
+
+const isSpotted = (birdType) => {
+  return birdType === 'Globby' || birdType === 'Bloggy';
+}
