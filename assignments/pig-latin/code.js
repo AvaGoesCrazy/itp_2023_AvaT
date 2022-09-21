7db949df44211e892a3fc6ae5d967d333f194e84
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2022-09-21 15:09:09 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -38,5 +38,5 @@ const pigLatin = (s) => s.substring(s.search(/[aeiou]/)) + s.substring(0, s.sear
 
 const advancedPigLatin = (s) => {
   const i = s.search(/[aeiou]/) 
-  return i === 0 ? s + 'way' : pigLatin;
+  return i === 0 ? s + 'way' : pigLatin(s);
 }
