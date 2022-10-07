e3f2e59db20ebfcb7a7f696b6bf8099e57dac144
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2022-10-07 15:15:04 -0700

    Updating assessments/unit-01-numeric/code.js

--- assessments/unit-01-numeric/code.js
+++ assessments/unit-01-numeric/code.js
@@ -7,3 +7,22 @@ const G = 6.6743e-11;
 // syntactically correct function skeleton, more credit for a skeleton with a
 // reasonable argument list, yet more credit for a working function, and full
 // credit for a clear and simple working function.
+const itemsLeftOver = (numPeople, numItems) => numItems - numPeople;
+
+const areaOfCircle = (radius) => Math.pi(radius) ** 2;
+
+const volumeOfCube = (edge) => edge ** 3;
+
+const populationGrowth = (popSize, growthRate) => popSize * (growthRate + 1);
+
+const earnedRunAverage = (earnedRuns, innings) => earnedRuns * innings * 9;
+
+const valueOfJewels = (numDiam, numEmer, diamInGold, emerInGold) => (numDiam * diamInGold) + (numEmer * emerInGold);
+
+const payWithOvertime = (hrsWorked, normalRate, overtimeRate) => (8 * normalRate) + ((hrsWorked - 8) * overtimeRate);
+
+const firstClassPostage = (weight) => (((Math.ceil(weight)) - 1) * .24) + .60;
+
+const weightOnJupiter = (weight) => (JUPITER_GRAVITY / EARTH_GRAVITY) * weight;
+
+const gravity = (mass1, mass2, distance) => ((mass1 / (distance ** 2)) * G) + ((mass2 / (distance ** 2)) * G);
