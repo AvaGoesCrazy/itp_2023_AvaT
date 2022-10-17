d69edc4dd76a5f7b43136fb436c4e841900cdfe9
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2022-10-17 15:28:37 -0700

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -9,3 +9,31 @@ const rand = (n) => Math.floor(Math.random() * n);
 // syntactically correct function skeleton, more credit for a skeleton with a
 // reasonable argument list, yet more credit for a working function, and full
 // credit for a clear and simple working function.
+
+const fireAlarm = (alarmPulled, smokeDetected, plannedDrill) => {
+  if (alarmPulled || smokeDetected || plannedDrill)
+    return true
+  else
+    return false
+}
+
+const canBePresident = (age, isNatCitizen, yearsOfRes) => {
+  if (age > 30 && isNatCitizen && yearsOfRes > 14)
+    return true
+  else
+    return false
+}
+
+const willSeeTweet = (followsTweeter, followsRetweeter, isBlocked) => {
+  if (followsTweeter || followsRetweeter && !isBlocked)
+    return true
+  else 
+    return false
+}
+
+const evenGreaterThanZero = (num) => {
+  if (num > 0)
+    return true
+  else
+    return false
+}
