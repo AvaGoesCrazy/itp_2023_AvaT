5adffa69e926156217a2918ddd29efecdc4bbe85
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2022-10-25 15:01:58 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -5,7 +5,7 @@
 // is called an anonymous function. We'll discuss this in more detail in a few
 // weeks but for now you can just adapt this code.
 
-drawLine(100, 100, 100, 100, 'black', 10)
+drawLine(100, 100, 200, 200, 'black', 10)
 
 let i = 0
 
