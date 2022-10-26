9d4936a190d0369b120be6f59adfbe78b824c2ef
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2022-10-26 14:58:02 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -20,30 +20,44 @@ const ticTacToe = [
 
 registerOnclick((x, y) => {
   if (x > 0 && x < 100 && y > 0 && y < 100)
-    if (i === 0){
+    if (i === 0) {
       drawText('X', 25, 60, 'black', Math.min(width, height) * 0.2);
       i = i + 1
-      ticTacToe [0] [0] = 'x'
+      ticTacToe[0][0] = 'x'
       console.log(i)
       console.log(ticTacToe)
     } else {
       drawText('O', 25, 60, 'black', Math.min(width, height) * 0.2);
       i = i - 1
-      ticTacToe [0] [0] = 'o'
+      ticTacToe[0][0] = 'o'
       console.log(i)
       console.log(ticTacToe)
     }
   if (x > 100 && x < 200 && y > 0 && y < 100)
-    if (i === 0){
+    if (i === 0) {
       drawText('X', 125, 60, 'black', Math.min(width, height) * 0.2);
       i = i + 1
-      ticTacToe [0] [1] = 'x'
+      ticTacToe[0][1] = 'x'
       console.log(i)
       console.log(ticTacToe)
     } else {
       drawText('O', 125, 60, 'black', Math.min(width, height) * 0.2);
       i = i - 1
-      ticTacToe [0] [1] = 'o'
+      ticTacToe[0][1] = 'o'
+      console.log(i)
+      console.log(ticTacToe)
+    }
+  if (x > 200 && x < 300 && y > 0 && y < 100)
+    if (i === 0) {
+      drawText('X', 225, 60, 'black', Math.min(width, height) * 0.2);
+      i = i + 1
+      ticTacToe[0][1] = 'x'
+      console.log(i)
+      console.log(ticTacToe)
+    } else {
+      drawText('O', 225, 60, 'black', Math.min(width, height) * 0.2);
+      i = i - 1
+      ticTacToe[0][1] = 'o'
       console.log(i)
       console.log(ticTacToe)
     }
