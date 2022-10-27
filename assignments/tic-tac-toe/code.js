fb1b1e4be06fca3e0ce54fb6153ea56016406574
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2022-10-27 14:39:51 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -20,7 +20,7 @@ const ticTacToe = [
 
 registerOnclick((x, y) => {
   if (x > 0 && x < 100 && y > 0 && y < 100)
-    if (ticTacToe [0] [0] !== 'x' || ticTacToe [0] [0] !== 'o')
+    if (ticTacToe [0] [0] !== ''){
       if (i === 0) {
         drawText('X', 25, 60, 'black', Math.min(width, height) * 0.2);
         i = i + 1
@@ -34,6 +34,7 @@ registerOnclick((x, y) => {
         console.log(i)
         console.log(ticTacToe)
       }
+    }
   if (x > 100 && x < 200 && y > 0 && y < 100)
     if (ticTacToe [0] [1] !== 'x' || ticTacToe [0] [1] !== 'o')
      if (i === 0) {
