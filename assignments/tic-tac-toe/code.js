b3eb9eddba26fda04f7f560884526be1878619c7
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2022-10-28 16:43:34 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -18,18 +18,19 @@ const ticTacToe = [
   ['', '', ''],
 ];
 
-const threeInARow = (ticTacToe) => {
-  if ((ticTacToe [0] [0] && ticTacToe [0] [1] && ticTacToe [0] [2] === 'x') || (ticTacToe [0] [0] && ticTacToe [0] [1] && ticTacToe [0] [2] === 'o')){
-    drawLine (50, 50, 250, 50 ,'black' , 10)
-  } else
-      if ((ticTacToe [1] [0] && ticTacToe [1] [1] && ticTacToe [1] [2] === 'x') || (ticTacToe [1] [0] && ticTacToe [1] [1] && ticTacToe [1] [2] === 'o')){
-      drawLine (50, 150, 250, 150 ,'black' , 10)
-      } else
-          if((ticTacToe [2] [0] && ticTacToe [2] [1] && ticTacToe [2] [2] === 'x') || (ticTacToe [2] [0] && ticTacToe [2] [1] && ticTacToe [2] [2] === 'o')){
-          drawLine (50, 250, 250, 250 ,'black' , 10)
-          }
-              
-}
+//WIN DETECTION DOES NOT WORK YET
+//const threeInARow = (ticTacToe) => {
+//  if ((ticTacToe [0] [0] && ticTacToe [0] [1] && ticTacToe [0] [2] === 'x') || (ticTacToe [0] [0] && ticTacToe [0] [1] && ticTacToe [0] [2] === 'o')){
+//    drawLine (50, 50, 250, 50 ,'black' , 10)
+//  } else
+//      if ((ticTacToe [1] [0] && ticTacToe [1] [1] && ticTacToe [1] [2] === 'x') || (ticTacToe [1] [0] && ticTacToe [1] [1] && ticTacToe [1] [2] === 'o')){
+//      drawLine (50, 150, 250, 150 ,'black' , 10)
+//      } else
+//          if((ticTacToe [2] [0] && ticTacToe [2] [1] && ticTacToe [2] [2] === 'x') || (ticTacToe [2] [0] && ticTacToe [2] [1] && ticTacToe [2] [2] === 'o')){
+//          drawLine (50, 250, 250, 250 ,'black' , 10)
+//         }
+//              
+//}
 
 registerOnclick((x, y) => {
   if (x > 0 && x < 100 && y > 0 && y < 100)
