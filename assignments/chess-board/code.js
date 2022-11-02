177fc697edd849491f274a9ca380e7533bd9ce75
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2022-11-02 14:49:11 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -12,6 +12,7 @@ const BLACK_KNIGHT = '♞';
 const BLACK_PAWN   = '♟';
 
 // Example of drawing one of the pieces
+//SECTION 1
 drawFilledRect(0, 0, 50, 50, 'black')
 drawFilledRect(100, 0, 50, 50, 'black')
 drawFilledRect(200, 0, 50, 50, 'black')
@@ -20,6 +21,7 @@ drawFilledRect(50, 50, 50, 50, 'black')
 drawFilledRect(150, 50, 50, 50, 'black')
 drawFilledRect(250, 50, 50, 50, 'black')
 drawFilledRect(350, 50, 50, 50, 'black')
+//SECTION 2
 drawFilledRect(0, 100, 50, 50, 'black')
 drawFilledRect(100, 100, 50, 50, 'black')
 drawFilledRect(200, 100, 50, 50, 'black')
@@ -28,5 +30,23 @@ drawFilledRect(50, 150, 50, 50, 'black')
 drawFilledRect(150, 150, 50, 50, 'black')
 drawFilledRect(250, 150, 50, 50, 'black')
 drawFilledRect(350, 150, 50, 50, 'black')
+//SECTION 3
+drawFilledRect(0, 200, 50, 50, 'black')
+drawFilledRect(100, 200, 50, 50, 'black')
+drawFilledRect(200, 200, 50, 50, 'black')
+drawFilledRect(300, 200, 50, 50, 'black')
+drawFilledRect(50, 250, 50, 50, 'black')
+drawFilledRect(150, 250, 50, 50, 'black')
+drawFilledRect(250, 250, 50, 50, 'black')
+drawFilledRect(350, 250, 50, 50, 'black')
+//SECTION 4
+drawFilledRect(0, 300, 50, 50, 'black')
+drawFilledRect(100, 300, 50, 50, 'black')
+drawFilledRect(200, 300, 50, 50, 'black')
+drawFilledRect(300, 300, 50, 50, 'black')
+drawFilledRect(50, 350, 50, 50, 'black')
+drawFilledRect(150, 350, 50, 50, 'black')
+drawFilledRect(250, 350, 50, 50, 'black')
+drawFilledRect(350, 350, 50, 50, 'black')
 
 drawText(WHITE_KING, width/2, height/2, 'black', 64);
