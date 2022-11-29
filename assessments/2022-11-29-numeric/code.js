18dd6af0d6653e95b6ffe9ae7c37277d61413819
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2022-11-29 14:47:26 -0800

    Updating assessments/2022-11-29-numeric/code.js

--- assessments/2022-11-29-numeric/code.js
+++ assessments/2022-11-29-numeric/code.js
@@ -3,3 +3,11 @@ const totalEggs = (h, s) => h + s;
 const chocolatesPerPerson = (c, p) => (c - (c % p)) / p;
 
 const extraChocolates = (c, p) => c % p;
+
+const leftOut = (c, p) => {
+  if (c > p){
+    return 0
+  } else {
+    return p - c
+  }
+}
