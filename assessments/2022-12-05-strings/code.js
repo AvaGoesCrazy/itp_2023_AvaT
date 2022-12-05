c3942dbda5f5e6d610fae0d982b90541a160ce23
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2022-12-05 15:12:49 -0800

    Updating assessments/2022-12-05-strings/code.js

--- assessments/2022-12-05-strings/code.js
+++ assessments/2022-12-05-strings/code.js
@@ -4,11 +4,13 @@ const charactersAround = (s, i) => s[i - 1] + s[i + 1];
 
 const middle = (s) => s.substring(s.length / 4, s.length - (s.length / 4));
 
-const pair = (s, i) => s + ' and ' + i
+const pair = (s, i) => s + ' and ' + i;
 
 const containsX = (s) => {
   if (s.indexOf('x' === -1)) {
     return false
   } else
     return true
-}
+};
+
+const slug = (x, y, z) => x.toLowerCase() + '-' + y.toLowerCase() + '-' + z.toLowerCase
