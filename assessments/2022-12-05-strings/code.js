84417243ef13d3eb1e463b10c85879ab8c2bf4fe
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2022-12-05 15:05:52 -0800

    Updating assessments/2022-12-05-strings/code.js

--- assessments/2022-12-05-strings/code.js
+++ assessments/2022-12-05-strings/code.js
@@ -7,3 +7,10 @@ const charactersAround = (s, i) => s[s.indexOf(i) - 1] + s[s.indexOf(i) + 1];
 const middle = (s) => s.substring(s.length / 4, s.length - (s.length / 4));
 
 const pair = (s, i) => s + ' and ' + i
+
+const containsX = (s) => {
+  if (s.lndexOf('x' === -1)) {
+    return false
+  } else
+    return true
+}
