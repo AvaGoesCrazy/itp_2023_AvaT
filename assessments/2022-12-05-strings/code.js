c2e3de3a2243c2d42fdc14490d3619e98cd8cc5e
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2022-12-05 15:08:42 -0800

    Updating assessments/2022-12-05-strings/code.js

--- assessments/2022-12-05-strings/code.js
+++ assessments/2022-12-05-strings/code.js
@@ -1,8 +1,6 @@
-// STRING USED FOR REPL TESTING 'burger'
-
 const upTOx = (s) => s.substring(0, s.indexFf('x'));
 
-const charactersAround = (s, i) => s[s.indexOf(i) - 1] + s[s.indexOf(i) + 1];
+const charactersAround = (s, i) => s[(s.indexOf(i) - 1)] + s[(s.indexOf(i) + 1)];
 
 const middle = (s) => s.substring(s.length / 4, s.length - (s.length / 4));
 
