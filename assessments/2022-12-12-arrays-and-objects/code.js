7fbf099ff374498f3840991a8512388b14dd878d
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2022-12-12 15:17:11 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -18,3 +18,10 @@ let move = makeMove('X', 1, 1)
 const placeMark = (board, move) => {
   board[move.row] [move.column] = move.mark
 }
+
+let ary = ['x', 'x', 'x']
+
+const allTheSame = (ary) => {
+  if (ary[0] === ary[1] && ary[0] === ary[2])
+    return ary;
+}
