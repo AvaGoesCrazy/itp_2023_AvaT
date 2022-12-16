ae0cdf0ccdf7678a3c96358df0abf6600804c3f9
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2022-12-16 11:05:55 -0800

    Updating assessments/2022-10-31-strings/code.js

--- assessments/2022-10-31-strings/code.js
+++ assessments/2022-10-31-strings/code.js
@@ -24,3 +24,5 @@ const upDownLastCharacter = (s) => s[s.length - 1].toUpperCase() + s[s.length - 
 const firstAndLast = (s) => s[0] + s[s.length - 1];
 
 const swapFrontAndBack = (s) => s.substring(s.length / 2) + s.substring(0, (s.length / 2));
+
+const simplePigLatin = (s, x) => s.substring(x) + s.substring(0, x) + 'ay';
