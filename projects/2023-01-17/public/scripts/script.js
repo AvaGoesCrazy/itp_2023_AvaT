5d366f9d4a515b88fd24807d4e70102ec6467f7d
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-01-18 23:33:04 +0000

    the stuff i worked on today.

--- projects/2023-01-17/public/scripts/script.js
+++ projects/2023-01-17/public/scripts/script.js
@@ -11,6 +11,39 @@ const a1 = document.createElement('p');
 const a2 = document.createElement('p');
 const a3 = document.createElement('p');
 
+const list = document.createElement('ol');
+const le1a = document.createElement('li');
+const le2a = document.createElement('li');
+const le3a = document.createElement('li');
+const le4a = document.createElement('li');
+const le5a = document.createElement('li');
+const le6a = document.createElement('li');
+const le7a = document.createElement('li');
+const le8a = document.createElement('li');
+const le9a = document.createElement('li');
+const le10a = document.createElement('li');
+const le1b = document.createElement('li');
+const le2b = document.createElement('li');
+const le3b = document.createElement('li');
+const le4b = document.createElement('li');
+const le5b = document.createElement('li');
+const le6b = document.createElement('li');
+const le7b = document.createElement('li');
+const le8b = document.createElement('li');
+const le9b = document.createElement('li');
+const le10b = document.createElement('li');
+
+const co1 = document.createElement('code');
+const co2 = document.createElement('code');
+const co3 = document.createElement('code');
+const co4 = document.createElement('code');
+const co5 = document.createElement('code');
+const co6 = document.createElement('code');
+const co7 = document.createElement('code');
+const co8 = document.createElement('code');
+const co9 = document.createElement('code');
+const co10 = document.createElement('code');
+
 // Create a text node and add it to the H1
 q1.append(document.createTextNode('Can you tell me why HTML was developed?'));
 
@@ -48,9 +81,30 @@ a2.append(
 
 a3.append(
   document.createTextNode(`
- Some of the main elements of HTML include
+ Some of the main elements of HTML include:
 `),
 );
+
+le1a.append(document.createTextNode(`1.`),);
+le1b.append(document.createTextNode(``),);
+le2a.append(document.createTextNode(`2.`),);
+le2b.append(document.createTextNode(``),);
+le3a.append(document.createTextNode(`3.`),);
+le3b.append(document.createTextNode(``),);
+le4a.append(document.createTextNode(`4.`),);
+le4b.append(document.createTextNode(``),);
+le5a.append(document.createTextNode(`5.`),);
+le5b.append(document.createTextNode(``),);
+le6a.append(document.createTextNode(`6.`),);
+le6b.append(document.createTextNode(``),);
+le7a.append(document.createTextNode(`7.`),);
+le7b.append(document.createTextNode(``),);
+le8a.append(document.createTextNode(`8.`),);
+le8b.append(document.createTextNode(``),);
+le9a.append(document.createTextNode(`9.`),);
+le9b.append(document.createTextNode(``),);
+le10a.append(document.createTextNode(`10.`),);
+le10b.append(document.createTextNode(``),);
 
 // Add the new P element to the body.
 // Add the new H1 element to the body.
@@ -65,3 +119,18 @@ body.append(a2);
 body.append(q3);
 
 body.append(a3);
+
+body.append(le1a);
+body.append(le1b);
+body.append(le2a);
+body.append(le2b);
+body.append(le3a);
+body.append(le3b);
+body.append(le4a);
+body.append(le4b);
+body.append(le5a);
+body.append(le5b);
+body.append(le6a);
+body.append(le6b);
+body.append(le7a);
+body.append(le7b);
