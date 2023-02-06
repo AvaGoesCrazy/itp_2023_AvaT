b06919c8b699c9e92b5ae130d013a80dee894274
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-02-06 23:31:13 +0000

    02062023

--- projects/web-puzzle/public/door1/script.js
+++ projects/web-puzzle/public/door1/script.js
@@ -1,28 +1,58 @@
-const randomRGB = () =>
-  Array(3)
-    .fill()
-    .map(() => Math.floor(Math.random() * 255));
+
 
 const rgb = (r, g, b) => `rgb(${r}, ${g}, ${b})`;
 
-const setRandomColor = (element) => {
-  const [r, g, b] = randomRGB();
+const mazeColor = (element) => {
   element.style.background = rgb(r, g, b);
-  element.style.color = rgb(255 - r, 255 - g, 255 - b);
+  element.style.color = rgb(0, 0, 255);
 };
+
+
+const maze = [
+  [0,1,1,1,0,0,0,0,0,0],
+  [0,1,0,1,0,0,0,0,0,0],
+  [0,1,0,1,0,0,1,1,1,0],
+  [0,1,0,1,1,1,1,0,1,0],
+  [0,1,0,0,0,0,0,0,1,0],
+  [0,1,1,1,0,1,1,1,1,0],
+  [0,0,0,1,0,1,0,0,0,0],
+  [1,1,0,1,0,1,0,0,1,1],
+  [0,1,1,1,0,1,1,0,1,0],
+  [0,0,0,0,0,0,1,1,1,0]
+]
+
+let isMazeClicked = [
+  ['n','n','n','n','n','n','n','n','n','n'],
+  ['n','n','n','n','n','n','n','n','n','n'],
+  ['n','n','n','n','n','n','n','n','n','n'],
+  ['n','n','n','n','n','n','n','n','n','n'],
+  ['n','n','n','n','n','n','n','n','n','n'],
+  ['n','n','n','n','n','n','n','n','n','n'],
+  ['n','n','n','n','n','n','n','n','n','n'],
+  ['n','n','n','n','n','n','n','n','n','n'],
+  ['n','n','n','n','n','n','n','n','n','n'],
+  ['n','n','n','n','n','n','n','n','n','n']
+]
+
+const tileType = (x, y) =>{
+  return maze[y] [x]
+}
+
+const isClicked = (x, y) =>{
+  if (isMazeClicked[y] [x] == 'y'){
+    return true
+  } else {
+    return false
+  }
+}
 
 const grid = document.querySelector('#grid');
 
 for (let i = 0; i < 10 * 10; i++) {
   const div = document.createElement('div');
   div.classList.add('box');
-  setRandomColor(div);
-  div.onclick = (e) => setRandomColor(e.target);
+  mazeColor(div);
+  div.onclick = (e) => mazeColor(e.target);
   grid.append(div);
 }
 
-document.querySelector('button').onclick = (e) => {
-  grid.querySelectorAll('div').forEach((div) => {
-    setRandomColor(div);
-  });
-};
