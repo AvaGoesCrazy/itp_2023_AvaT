47f98b29e5102fcdc5500edec386a0c6881ee4f5
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-02-24 15:06:51 -0800

    Updating assessments/2023-02-24-refactoring/code.js

--- assessments/2023-02-24-refactoring/code.js
+++ assessments/2023-02-24-refactoring/code.js
@@ -12,7 +12,7 @@ const drawPicture = (horizon, base, size) => {
       drawFilledCircle(x, y, rad, colorIn, lThick)
     }
     for (let i = 0; i < numSeg - 1; i++){
-      drawTwoO(x, y, rad-(rad*(i*.25)), colorIn, colorOut, lThick)
+      drawTwoO(x, y - (i*rad), rad-(rad*(i*.25)), colorIn, colorOut, lThick)
     }
   }
 
