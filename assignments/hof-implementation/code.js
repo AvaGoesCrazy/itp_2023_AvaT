c6a570f0b09f078b9b79a20eb9b4e3d820e21c29
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-02-27 15:24:37 -0800

    Updating assignments/hof-implementation/code.js

--- assignments/hof-implementation/code.js
+++ assignments/hof-implementation/code.js
@@ -1,7 +1,7 @@
 // N.B. Do not use the array methods of the same name to implement these functions!
 
 const filter = (pre, ary) => {
-  ary.filter = (pre)
+  ary.filter = (pre);
 };
 
 const map = (fn, array) => {
