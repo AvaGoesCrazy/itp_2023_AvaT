118f03c120bef87ce02ca74f6584e856ecf77faa
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-02-28 15:22:28 -0800

    Updating assignments/hof-implementation/code.js

--- assignments/hof-implementation/code.js
+++ assignments/hof-implementation/code.js
@@ -23,7 +23,9 @@ const map = (fn, ary) => {
 const flatMap = (fn, ary) => {
   let ary2 = []
   for (let i = 0; i < ary.length; i++){
-    if(fn(ary[i]) !== []){
+    if(fn(ary[i]) === []){
+      
+    } else{
       ary2.push(fn(ary[i]))
     }
   }
