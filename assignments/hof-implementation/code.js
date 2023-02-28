9416bfe58b2af1fb0181d51d840f04bc9c4cdd32
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-02-28 14:49:58 -0800

    Updating assignments/hof-implementation/code.js

--- assignments/hof-implementation/code.js
+++ assignments/hof-implementation/code.js
@@ -1,7 +1,7 @@
 // N.B. Do not use the array methods of the same name to implement these functions!
 
 const filter = (pre, ary) => {
-  let ary2 = ['']
+  let ary2 = []
   for (let i = 0; i > ary.length; i++){
     if (pre(ary[i - 1])){
       ary2.push(ary[i])
