0d8a8345411d38c82ea7724f6b2383d9a8c39ee1
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-03-02 15:23:12 -0800

    Updating assignments/hof-implementation/code.js

--- assignments/hof-implementation/code.js
+++ assignments/hof-implementation/code.js
@@ -41,7 +41,7 @@ const reduce = (fn, initVal, ary) => {
 
 const every = (pred, ary) => {
   for (let i = 0; i < ary.length; i++)
-   return pred(ary[i])
+   return pred(ary[i - 1])
 };
 
 const some = (predicate, array) => {
