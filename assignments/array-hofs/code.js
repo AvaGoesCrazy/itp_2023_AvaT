5d666f76f3971a3e0e06d6156e64642fcf44cba7
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-03-03 14:43:58 -0800

    Updating assignments/array-hofs/code.js

--- assignments/array-hofs/code.js
+++ assignments/array-hofs/code.js
@@ -1,3 +1,61 @@
+const filter = (pre, ary) => {
+  let ary2 = []
+  for (let i = 0; i < ary.length; i++){
+    if (pre(ary[i])){
+      ary2.push(ary[i])
+    }
+  }
+  return ary2
+};
+
+const map = (fn, ary) => {
+  let ary2 = []
+  for (let i = 0; i < ary.length; i++){
+    ary2.push(fn(ary[i]))
+  }
+  return ary2
+};
+
+const flatMap = (fn, ary) => {
+  let ary2 = []
+  for (let i = 1; i < ary.length + 1; i++){
+    for (let n = 0; n < fn(i).length; n++){
+      ary2.push(fn(i)[n])
+    }
+  }
+  return ary2
+};
+
+const reduce = (fn, initVal, ary) => {
+  let lastNum = initVal
+  for (let i = 0; i < ary.length; i++){
+    lastNum = fn(lastNum, ary[i])
+  }
+  return lastNum
+};
+
+const every = (pred, ary) => {
+  let trueCount = 0
+  for (let i = 0; i < ary.length; i++){
+    if (pred(ary[i])){
+      trueCount ++
+    }
+  }
+  return trueCount === ary.length
+};
+
+const some = (pred, ary) => {
+  let trueCount = 0
+  for (let i = 0; i < ary.length; i++){
+    if (pred(ary[i])){
+      trueCount ++
+    }
+  }
+  return trueCount !== 0
+};
+
+//-------  Actual functions start here: ---------
+
 const evens = (ary) =>{
   filter((n) => n % 2 === 0, ary)
 }
