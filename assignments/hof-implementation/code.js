232d0b85589164fe057a37c89963dce5ee29910a
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-03-07 14:51:44 -0800

    Updating assignments/hof-implementation/code.js

--- assignments/hof-implementation/code.js
+++ assignments/hof-implementation/code.js
@@ -21,8 +21,10 @@ const map = (fn, ary) => {
 // I worked with my table partner on this one, we got it to work but neither of us could figure out why.
 const flatMap = (fn, ary) => {
   let ary2 = []
-  for (let i = 0; i < ary.length; i++){
-    ary2[i] = fn(ary[i])
+  for (let i = 1; i < ary.length + 1; i++){
+    for (let n = 0; n < fn(i).length; n++){
+      ary2.push(fn(i)[n])
+    }
   }
   return ary2
 };
