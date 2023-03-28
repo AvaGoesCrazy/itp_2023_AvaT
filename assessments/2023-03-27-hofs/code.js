291a42d3e36f1d8019f8c501913df660204c1430
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-03-28 14:42:38 -0700

    Updating assessments/2023-03-27-hofs/code.js

--- assessments/2023-03-27-hofs/code.js
+++ assessments/2023-03-27-hofs/code.js
@@ -1,3 +1,62 @@
+const filter = (pre, ary) => {
+  let ary2 = []
+  for (let i = 0; i < ary.length; i++){
+    if (pre(ary[i])){
+      ary2.push(ary[i])
+    }
+  }
+  return ary2
+};
+
+const map = (fn, ary) => {
+  let ary2 = []
+  for (let i = 0; i < ary.length; i++){
+    ary2.push(fn(ary[i]))
+  }
+  return ary2
+};
+
+const flatMap = (fn, ary) => {
+  let ary2 = []
+  for (let i = 0; i < ary.length; i++){
+    for (let j = 0; j < fn(ary[i]).length; j++){
+      ary2.push(fn(ary[i])[j])
+    }
+  }
+  return ary2
+};
+
+const reduce = (fn, initVal, ary) => {
+  let lastNum = initVal
+  for (let i = 0; i < ary.length; i++){
+    lastNum = fn(lastNum, ary[i])
+  }
+  return lastNum
+};
+
+const every = (pred, ary) => {
+  let trueCount = 0
+  for (let i = 0; i < ary.length; i++){
+    if (pred(ary[i])){
+      trueCount ++
+    } else{
+      return false
+    }
+  }
+  return trueCount === ary.length
+};
+
+const some = (pred, ary) => {
+  let trueCount = 0
+  for (let i = 0; i < ary.length; i++){
+    if (pred(ary[i])){
+      trueCount ++
+    }
+  }
+  return trueCount !== 0
+};
+// Idk why i need to copypaste my HOF implements functions to get it wo work but here they are ^^^
+
 const fruitBowl = [
   {
     name: 'apple',
