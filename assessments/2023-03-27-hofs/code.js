4cdced3a9e4af0bdef7b0c4c5a68a6f584f4d5cf
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-03-28 14:49:59 -0700

    Updating assessments/2023-03-27-hofs/code.js

--- assessments/2023-03-27-hofs/code.js
+++ assessments/2023-03-27-hofs/code.js
@@ -169,7 +169,7 @@ const isTasty = (fruit) => fruit.tastiness >= 5;
 const isInconvenient = (fruit) => fruit.inconvenience >= 5;
 
 const redFruits = (fruits) => {
-  return filter((n) => n.colors.includes('red'), fruits)
+  return filter((n) => n.colors.includes('red') === true, fruits)
 }
 
 const weights = (fruits) => {
