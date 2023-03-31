cf84f7b9c29ccb17f41752c1d88f61abdb7a3eb7
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-03-31 21:50:58 +0000

    push snake changes

--- projects/snake/public/script.js
+++ projects/snake/public/script.js
@@ -1,5 +1,7 @@
 // TO DO:
-// - Make gui so i can actually see what im doing
+// - Make snake lose when coliding with self
+// - Make score counter
+// - let player input directions whenever they want, not just on a timer.
 
 
 const countDown = [
@@ -29,16 +31,16 @@ const snakeHead = [
 ];
 
 const apple = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false]
 ];
 
 const board = [
@@ -95,13 +97,15 @@ const newApple = () => {
     let randomX = Math.floor(Math.random() * 10)
     let randomY = Math.floor(Math.random() * 10)
     if(countDown[randomY] [randomX] === 0 && snakeHead[randomY] [randomX] === 'n') {
-      apple[randomY] [randomX] ++
+      apple[randomY] [randomX] = true
       canMakeApple = true
       makeRed(board[randomY] [randomX])
     }
   }
 }
 
+
+//Game starts here:
 document.body.onkeydown = (e) => {
   console.log('Game Start!')
 
@@ -110,27 +114,28 @@ document.body.onkeydown = (e) => {
   let X = 5
   let Y = 5
   
-  let bufferMove = 'up'
-  
+  let bufferMove = 'ArrowUp'
+  let previousMove = 'ArrowDown'
+
   let turn = 0
   
-  let score = 0
+  let score = 2
+
+  let moveDoneThisTurn = false
   
   newApple()
   makeBlue(board[Y] [X])
 
+  for(let i = 0; i< 10; i++){
+    for(let n = 0; n< 10; n++){
+      makeClear(board[Y] [X])
+    }
+  }
+
   document.body.onkeydown = (e) => {
-    if (e.key === 'ArrowUp') {
-      bufferMove = 'up'
-    }
-    if (e.key === 'ArrowDown') {
-      bufferMove = 'down'
-    }
-    if (e.key === 'ArrowLeft') {
-      bufferMove = 'left'
-    }
-    if (e.key === 'ArrowRight') {
-      bufferMove = 'right'
+    bufferMove = e.key
+    if(bufferMove !== e.key){
+      doMove(bufferMove)
     }
   }
 
@@ -155,20 +160,46 @@ document.body.onkeydown = (e) => {
   }
 
   const drawSnakeBody = () => {
-    for (let i = 0; i< countDown.length; i++) {
-      for (let n = 0; n < countDown.length; n++) {
+    for (let i = 0; i < 10; i++) {
+      for (let n = 0; n < 10; n++) {
         if (countDown[i] [n] > 0){
           makeGreen(board[i] [n])
         }
-        if (countDown[i] [n] === 0 && snakeHead[i] [n] === 'n' && apple[i] [n] === 0){
+        if (countDown[i] [n] === 0 && snakeHead[i] [n] === 'n' && !apple[i] [n]){
           makeClear(board[i] [n])
         }
       } 
     }
   }
 
-  const safeMove = () => ((Y++ && X++ < 10) && (Y-- && X-- > -1))
+  const getOpposite = (i) => {
+    if (i === 'ArrowUp'){
+      return 'ArrowDown'
+    }
+    if (i === 'ArrowDown'){
+      return 'ArrowUp'
+    }
+    if (i === 'ArrowLeft'){
+      return 'ArrowRight'
+    }
+    if (i === 'ArrowRight'){
+      return 'ArrowLeft'
+    }
+  }
 
+  const safeMove = () => {(
+    Y++ < 10 && 
+    X++ < 10 && 
+    Y-- > -1 && 
+    X-- > -1
+  ) && countDown[X] [Y] === 0
+  }
+
+  //const notGoingBack = () => {
+  //  if(bufferMove === )
+  //}
+
+  //When the snake moves:
   const movePart1 = () =>{
     snakeHead[Y] [X] = 'n'
     makeClear(board[Y] [X])
@@ -178,34 +209,35 @@ document.body.onkeydown = (e) => {
     snakeHead[Y] [X] = 'y'
     countDown[Y] [X] = score
 
-    makeBlue (board[Y] [X])
-    if (apple[Y] [X] = 1){
-      apple[Y] [X] --
+    if (apple[Y] [X]){
+      apple[Y] [X] = false
       score ++
-      newApple()
       addSegment()
+      newApple()
     }
+
     drawSnakeBody()
+    makeBlue (board[Y] [X])
     removeSegment()
   }
 
   const move = (dir) => {
-    if(dir === 'up' && safeMove()) {
+    if(dir === 'ArrowUp' && safeMove()) {
       movePart1()
       Y--
       movePart2()
     }
-    if(dir === 'down' && safeMove()) {
+    if(dir === 'ArrowDown' && safeMove()) {
       movePart1()
       Y++
       movePart2()
     }
-    if(dir === 'left' && safeMove()) {
+    if(dir === 'ArrowLeft' && safeMove()) {
       movePart1()
       X--
       movePart2()
     }
-    if(dir === 'right' && safeMove()) {
+    if(dir === 'ArrowRight' && safeMove()) {
       movePart1()
       X++
       movePart2()
@@ -216,88 +248,14 @@ document.body.onkeydown = (e) => {
     }
   }
 
+  //this thing just makes sure the game is actuallly stared when doing moves. Idk if it actually works how its suposed to though...
   const doMove = (dir) => {
     if (gameStarted) {
       move(dir)
-      console.log(countDown)
     }
   }
-
+  
+  // does a move every [speed * .001] of a second:
   setInterval(() => doMove(bufferMove), speed);
 }
 
-
-
-
-
-
-//const rgb = (r, g, b) => `rgb(${r}, ${g}, ${b})`;
-//
-//const makeBlue = (element) => {
-//  element.style.background = rgb(0, 100, 255);
-//  element.style.color = rgb(255, 155, 0);
-//};
-//const makeRed = (element) => {
-//  element.style.background = rgb(255, 0, 0);
-//  element.style.color = rgb(0, 255, 255);
-//};
-//const makeGreen = (element) => {
-//  element.style.background = rgb(0, 255, 0);
-//  element.style.color = rgb(255, 0, 255);
-//};
-//
-//const grid = document.querySelector('#grid');
-//
-//for (let i = 0; i < 10 * 10; i++) {
-//  const div = document.createElement('div');
-//  div.classList.add('box');
-//  makeBlue(div);
-//
-//  const inBounds = (i) => {
-//    return 9 >= Y(i) && Y(i) >= 0 && 9 >= X(i) && X(i) >= 0
-//  }
-//maybe if i define the fucntions here they'll work, that was partially the case but now its still weird
-//
-//  const clickedNearby = (e) => {
-//    return (
-//      isMazeClicked[Y(i) + 1][X(i)] === 'y' || 
-//      isMazeClicked[Y(i) - 1][X(i)] === 'y' || 
-//      isMazeClicked[Y(i)][X(i) + 1] === 'y' || 
-//      isMazeClicked[Y(i)][X(i) - 1] === 'y'
-//    ) 
-//  }
-//
-//  const greenNearby = (e) => {
-//    return (
-//      inBounds(maze[Y(i) + 1][X(i)]) === '1' || 
-//      inBounds(maze[Y(i) - 1][X(i)]) === '1' || 
-//      inBounds(maze[Y(i)][X(i) + 1]) === '1' || 
-//      inBounds(maze[Y(i)][X(i) - 1]) === '1'
-//    ) 
-//  }
-//
-//This is the stuff that happens when you click on a square
-//  div.onclick = (e) => {
-
-// click on a MAZE SQUARE:
-//    if (clickedNearby(e) && greenNearby(e) && maze[Y(i)][X(i)] === 1 && isMazeClicked[Y(i)][X(i)] === 'n') {
-//      isMazeClicked[Y(i)][X(i)] = 'y';
-//      makeGreen(e.target);
-//      console.log('you found a maze square!')
-//    } else {
-//
-//click on a BOUNDRY SQUARE:
-//      if (maze[Y(i)][X(i)] === 0 && isMazeClicked[Y(i)][X(i)] === 'n') {
-//        isMazeClicked[Y(i)][X(i)] = 'y';
-//        makeRed(e.target);
-//        console.log('you cant click here')
-//      } else {
-//
-// click on an already CLICKED SQUARE:
-//        if (isMazeClicked[Y(i)][X(i)] === 'y');
-//        console.log('you already clicked here')
-//      };
-//    };
-//  };
-//  grid.append(div);
-//};
