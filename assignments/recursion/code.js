8f660861b96d59cc533cb1a8b1832142ca84ff0b
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-04-11 14:45:16 -0700

    Updating assignments/recursion/code.js

--- assignments/recursion/code.js
+++ assignments/recursion/code.js
@@ -15,9 +15,13 @@ const triangular = (n) =>{
 }
 
 const fibonacci = (n) =>{
-    if(n === 0){
+  if(n === 0){
     return 0
-  } else {
+  }
+  if(n === 1){
+    return 1
+  }
+  if(n > 1){
     return fibonacci(n - 1) + fibonacci(n - 1)
   }
 }
