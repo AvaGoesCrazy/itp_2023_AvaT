30562e529f298da569e7783fd4da340e481cea4d
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-04-17 15:28:05 -0700

    Updating assignments/recursion-2/code.js

--- assignments/recursion-2/code.js
+++ assignments/recursion-2/code.js
@@ -33,3 +33,14 @@ const triple = (n1, n2) =>{
   }
 }
 
+const power = (n1, n2) =>{
+    if (n2 === 1){
+    return n1 * n1
+  } else{ 
+    if (n2 === 0){
+    return n1
+    } else{
+    return power(n1, n2 - 1) * n1
+    }
+  }
+}
