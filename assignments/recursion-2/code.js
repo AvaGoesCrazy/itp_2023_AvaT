0dfa0ea824197ce1ac569fc476dc369b53ce32d8
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-04-18 14:48:41 -0700

    Updating assignments/recursion-2/code.js

--- assignments/recursion-2/code.js
+++ assignments/recursion-2/code.js
@@ -25,11 +25,11 @@ const double = (n1, n2) =>{
     return n1 * 2
   } else{ 
     if (n2 === 0){
-    return n1
+      return n1
     } else{
-    return double(n1, n2 - 1) * 2
+        return double(n1, n2 - 1) * 2
+      }
     }
-  }
 }
 
 const triple = (n1, n2) =>{
@@ -37,11 +37,11 @@ const triple = (n1, n2) =>{
     return n1 * 3
   } else{ 
     if (n2 === 0){
-    return n1
+      return n1
     } else{
-    return triple(n1, n2 - 1) * 3
+        return triple(n1, n2 - 1) * 3
+      }
     }
-  }
 }
 
 const power = (n1, n2) =>{
@@ -49,9 +49,9 @@ const power = (n1, n2) =>{
     return n1
   } else{ 
     if (n2 === 0){
-    return 1
+      return 1
     } else{
-    return power(n1, n2 - 1) * n1
+        return power(n1, n2 - 1) * n1
+      }
     }
-  }
 }
