04bcb424b194ff2e656b5df8c4c7e76b955e725b
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-04-18 15:17:11 -0700

    Updating assignments/recursion-2/code.js

--- assignments/recursion-2/code.js
+++ assignments/recursion-2/code.js
@@ -86,7 +86,7 @@ const maximum = (a) =>{
   if (a === []){
     return 0
   } else{
-    if (a > maximum(a.slice(1))){
+    if (a[0] > maximum(a.slice(1))){
       return a[0]
     } else{
       return maximum(a.slice(1))
