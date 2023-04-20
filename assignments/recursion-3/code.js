d79b28aa03f1393480cdbc8542c19e6a608916cb
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-04-20 15:00:20 -0700

    Updating assignments/recursion-3/code.js

--- assignments/recursion-3/code.js
+++ assignments/recursion-3/code.js
@@ -11,13 +11,9 @@ const product = (n) =>{
 }
 
 const sumSquares = (n) =>{
-  if (n.length === 1) {
-    return n[0]
+  if (n === 1){
+    return n
   } else{
-    if (n.length === 0){
-      return 1
-    } else{
-        return n[0] ** product(n.slice(1))
-      }
-    }
+    return n ** sumSquares(n - 1)
+  }
 }
