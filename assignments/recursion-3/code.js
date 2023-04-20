4a6be88e7f24b4f13df92fa4c024dde4cca6d102
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-04-20 14:57:46 -0700

    Updating assignments/recursion-3/code.js

--- assignments/recursion-3/code.js
+++ assignments/recursion-3/code.js
@@ -2,8 +2,10 @@ const product = (n) =>{
   if (n.length === 1) {
     return n[0]
   } else{
-    return n[0] * product(n.slice(1))
-  }
+    if (n.length === 0){
+      return 1
+    } else{
+        return n[0] * product(n.slice(1))
+      }
+    }
 }
-
-console.log(81 * 81)
