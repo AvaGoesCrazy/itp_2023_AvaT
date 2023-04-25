b108e416700d4737b759dfca38f536ca0e9396fb
AvaGoesCrazy <112733118+AvaGoesCrazy@users.noreply.github.com>
2023-04-25 15:03:40 -0700

    Updating assessments/2023-04-21-recursion/code.js

--- assessments/2023-04-21-recursion/code.js
+++ assessments/2023-04-21-recursion/code.js
@@ -64,10 +64,10 @@ const nvwls = (s) =>{
     return ''
   } else{
     if(s[0] === 'a' ||
-    s[0] === 'e' ||
-    s[0] === 'i' ||
-    s[0] === 'o' ||
-    s[0] === 'u' ||){
+       s[0] === 'e' ||
+       s[0] === 'i' ||
+       s[0] === 'o' ||
+       s[0] === 'u'){
       return ''
     } else return s[0] + nvwls(s.substring(1))
   }
